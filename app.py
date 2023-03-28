18be16e4cf539e95d9cb17b5c0ee3a2890648f6c
Kayla Van Langen <78875468+krvanlangen@users.noreply.github.com>
2023-03-28 15:55:10 -0500

    commits

--- app.py
+++ app.py
@@ -3,13 +3,65 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+books_list = [
+    {"title": "The Song of Achilles", "author": "Madeline Miller ", "pages": "416", "classification": "fiction", "details": "read, recommend", "acquisition": "loaned"}
+]
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    return render_template(
+        "index.html", pageTitle="My library", books=books_list
+    )
 
-@app.route('/mike')
-def mike():
-    return render_template('mike.html')
 
-if __name__ == '__main__':
+@app.route("/add", methods=["POST"])
+def add():
+    print("inside add function")
+    if request.method == "POST":
+
+        form = request.form
+
+        title = form["title"]
+        author = form["author"]
+        pages = form["pages"]
+        classification = form["classification"]
+        details = form.getlist("details")
+        acquisition = form["acquisition"]
+        print(title)
+        print(author)
+        print(pages)
+        print(classification)
+        print(details)
+        print(acquisition)
+
+        details_string=", ".join(details)
+
+        add_book_dict  = {
+            "title": title,
+            "author": author,
+            "pages": pages,
+            "classification": classification,
+            "details": details_string,
+            "acquisition": acquisition,
+
+        }
+        print(add_book_dict)
+        books_list.append(add_book_dict)
+        print(books_list)
+        return redirect(url_for("index"))
+    else:
+        return redirect(url_for("index"))
+
+
+
+@app.route("/about", methods=["GET"])
+def about():
+    return render_template(
+        "about.html", pageTitle="About My Library"
+    )
+
+
+if __name__ == "__main__":
     app.run(debug=True)
